fb64457fa80a2084eed55590fa8f6c6ff0745e36
KizKnife <134030696+KizKnife@users.noreply.github.com>
2026-04-14 11:39:34 -0500

    Created constructor, getter, and setter methods in new CellPhone.java class and created user input for new CellPhone instance in main CellPhoneApplication.java

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,43 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // New CellPhone instance named 'phone'
+        CellPhone phone = new CellPhone();
+
+        // User input serial number, model, carrier, phone number, owner
+        System.out.print("What is the serial number? ");
+        int serialNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("What model is the phone? ");
+        String model = scanner.nextLine();
+
+        System.out.print("Who is the carrier? ");
+        String carrier = scanner.nextLine();
+
+        System.out.print("What is the phone number? ");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.print("Who is the owner of the phone? ");
+        String owner = scanner.nextLine();
+
+        // Setters
+        phone.setSerialNumber(serialNumber);
+        phone.setModel(model);
+        phone.setCarrier(carrier);
+        phone.setPhoneNumber(phoneNumber);
+        phone.setOwner(owner);
+
+        // Getters
+        System.out.printf("%nSerial Number: %d%n", phone.getSerialNumber());
+        System.out.printf("Model: %s%n", phone.getModel());
+        System.out.printf("Carrier: %s%n", phone.getCarrier());
+        System.out.printf("Phone Number: %s%n", phone.getPhoneNumber());
+        System.out.printf("Owner: %s%n", phone.getOwner());
+    }
 }
